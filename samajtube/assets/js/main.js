5e0077eb72bc79cd2418cb884c6dd3a393eedec7
Sudhan Shahi <ss.dev.n06@gmail.com>
2021-11-21 15:58:28 +0545

    Completed all blocks

--- samajtube/assets/js/main.js
+++ samajtube/assets/js/main.js
@@ -1,4 +1,23 @@
 jQuery(document).ready(function ($) {
+
+    // Background Imgage
+    $('.bg-cover,[data-fix="image"]').each(function () {
+        var wrap = $(this),
+            image = wrap.find('>img');
+        if (image.attr('src')) {
+            if (wrap.data('fix') != 'image') {
+                image.hide();
+                wrap.css({
+                    'background-image': 'url(\'' + image.attr('src') + '\')'
+                });
+            } else {
+                wrap.find('.svg.img-fluid').css({
+                    'background-image': 'url(\'' + image.attr('src') + '\')'
+                });
+            }
+        }
+    });
+
     $(document).on('click', function () {
         $('#toggleForm, .user__lists').removeClass('show');
     });
@@ -45,4 +64,135 @@ jQuery(document).ready(function ($) {
     stickyHeader();
 
     setTimeout(stickyHeader, 1000);
+
+
+
+
+    //Sliders
+    // slider-col-1
+    $('.slider-col-1').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+    });
+
+    // slider-col-2
+    $('.slider-col-2').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                adaptiveHeight: true,
+            }
+        }]
+    });
+
+    // slider-col-3
+    $('.slider-col-3').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                adaptiveHeight: true,
+            }
+        }
+        ]
+    });
+
+    // slider-col-4
+    $('.slider-col-4').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+                adaptiveHeight: true,
+            }
+        }
+        ]
+    });
+
+    // slider-col-6
+    $('.slider-col-6').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+        ]
+    });
+
+    //Example
+    //Adding Options
+    var yourOption = [{
+        breakpoint: 2000,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    }];
+    //Options Init
+    $('.your-class').slick('slickSetOption', 'responsive', yourOption, true);
 });
